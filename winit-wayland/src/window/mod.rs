261cda54017f98a12dc55569c864430fe6770366
Kirill Chibisov <contact@kchibisov.com>
2026-04-04 23:59:33 +0900

    winit-wayland: use `ext-background-effect` if available
    A more modern protocol compared to the KDE one.

--- winit-wayland/src/window/mod.rs
+++ winit-wayland/src/window/mod.rs
@@ -128,7 +128,8 @@ impl Window {
         // Set transparency hint.
         window_state.set_transparent(attributes.transparent);
 
-        window_state.set_blur(attributes.blur);
+        // Set blur.
+        let _ = window_state.set_blur(attributes.blur);
 
         // Set the decorations hint.
         window_state.set_decorate(attributes.decorations);
@@ -491,7 +492,9 @@ impl CoreWindow for Window {
 
     #[inline]
     fn set_blur(&self, blur: bool) {
-        self.window_state.lock().unwrap().set_blur(blur);
+        if self.window_state.lock().unwrap().set_blur(blur) {
+            self.request_redraw();
+        }
     }
 
     #[inline]
